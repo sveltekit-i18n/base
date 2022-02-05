b77ae699228801a26af3c517c12098dd00677814
Jayk <20700514+jayk09@users.noreply.github.com>
2022-02-05 17:54:17 +0100

    Initial commit

---
+++ src/types.d.ts
@@ -0,0 +1,50 @@
+import { Readable } from 'svelte/store';
+
+export type LoadingStore = Readable<boolean> & { toPromise: () => Promise<void[]>, get: () => boolean };
+
+export type Loader = () => Promise<Record<any, any>>;
+
+export type Route = string | RegExp;
+
+export type LoaderModule = {
+  key: string;
+  locale: string;
+  routes?: Route[];
+  loader: Loader;
+};
+
+export type DotNotationInput = Record<string, any> | null | undefined | any;
+
+export type DotNotationOutput = Record<string, any>;
+
+export type ToDotNotation = (input: DotNotationInput, parentKey?: string) => DotNotationOutput;
+
+export type FetchTranslations = (loaders: LoaderModule[]) => Promise<Record<string, DotNotationOutput>>;
+
+export type TranslationFunction = (key: string, vars?: Record<any, any>) => string;
+
+export type LocalTranslationFunction = (locale: string, key: string, vars?: Record<any, any>) => string;
+
+export type ExtendedStore<T, Get = () => T, Store = Readable<T>> = Store & { get: Get };
+
+export type ConfigTranslations = { [locale: string]: Record<string, any> };
+
+export type Translations = { [locale: string]: Record<string, string> };
+
+export type Config = {
+  parser: {
+    parse: (props: {
+      key: string;
+      payload: any;
+      translations: Translations;
+      locale: string;
+      fallbackLocale?: string;
+    }) => string;
+  };
+  loaders?: LoaderModule[];
+  translations?: ConfigTranslations;
+  initLocale?: string;
+  fallbackLocale?: string;
+};
+
+export type GetConfig = (...params: any) => Config;
