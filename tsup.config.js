accbfbdc8ef1256f94cf44e7bab112d01e80cb74
Jayk <20700514+jayk09@users.noreply.github.com>
2023-07-10 01:08:20 +0200

    Fix CommonJS build

--- tsup.config.js
+++ tsup.config.js
@@ -1,12 +1,16 @@
 /* eslint-disable */
 import { defineConfig } from 'tsup';
 
-export default defineConfig((options) => ({
-  clean: true,
-  dts: true,
-  format: ['cjs', 'esm'],
-  entry: ['src/index.ts'],
-  watch: options.watch && ['src/*'],
-  minify: !options.watch,
-  sourcemap: options.watch,
-}));
+export default defineConfig(
+  /** @type {() => import('tsup').Options} */
+  (options) => ({
+    clean: true,
+    dts: true,
+    format: ['cjs', 'esm'],
+    entry: ['src/index.ts'],
+    watch: options.watch && ['src/*'],
+    minify: !options.watch,
+    sourcemap: options.watch,
+    splitting: true,
+  }),
+);
